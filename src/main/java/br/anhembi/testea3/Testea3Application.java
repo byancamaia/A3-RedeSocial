4c065316fe6e95451931137b31f2ea44d0e149d0
Byanca Maia <byancacmaia@gmail.com>
2022-05-25 20:01:10 -0300

    fix: commit inicial - projeto criado

---
+++ src/main/java/br/anhembi/testea3/Testea3Application.java
@@ -0,0 +1,13 @@
+package br.anhembi.testea3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testea3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testea3Application.class, args);
+	}
+
+}
