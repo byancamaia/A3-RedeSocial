d30b6c5beadcafeac83c68a4610e55994760418f
byancacmaia@gmail.com <byancamaia@ibm.com>
2022-06-04 13:56:23 -0300

    usando padrao DTO para retornar usuarios

--- src/main/java/br/anhembi/testea3/resources/UserResource.java
+++ src/main/java/br/anhembi/testea3/resources/UserResource.java
@@ -2,6 +2,7 @@ package br.anhembi.testea3.resources;
 
 import br.anhembi.testea3.domain.User;
 
+import br.anhembi.testea3.dto.UserDTO;
 import br.anhembi.testea3.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -23,9 +25,10 @@ public class UserResource {
 
     //@GetMapping
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
